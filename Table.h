548f294f43e3893a624882e921552a446e7a0a06
Alexandre Abreu <up201800168@up.pt>
2019-10-27 12:37:32 +0000

    Table working with templates

--- Table.h
+++ Table.h
@@ -3,8 +3,11 @@
 #include <vector>
 #include <string>
 #include <sstream>
-
+const unsigned char PADDING = 3;
 using namespace std;
+
+template <class Printable>
+string normalize(Printable s, unsigned n);
 
 template <class Printable>
 class Table{
@@ -25,20 +28,32 @@ class Table{
                 sizes.push_back(aux.str().size());
             }
             for(auto row: table.getContent()){
-                for(unsigned i = 0; i < sizes.size(); ++i){
+                for(unsigned long i = 0; i < sizes.size(); ++i){
                     ostringstream aux;
                     aux << row.at(i);
                     if(aux.str().size() > sizes.at(i))
                         sizes.at(i) = aux.str().size();
                 }
             }
-            for(auto x : sizes)
-                out << x << endl;
+            string line = "+";
+            for (unsigned size : sizes){
+                line += string((size + PADDING * 2), '-') + "+";
+            }
+            out << line << endl;
+            for(unsigned i = 0; i < table.header.size(); ++i){
+                out << "|" << string(PADDING, ' ') << normalize(table.header.at(i), sizes.at(i)) << string(PADDING, ' ');
+            }
+            out << "|" << endl << line << endl;
+            for(unsigned i = 0; i < table.content.size(); ++i){
+                for(unsigned long j = 0; j < sizes.size(); ++j){
+                    out << "|" << string(PADDING, ' ') << normalize(table.content.at(i).at(j), sizes.at(j)) << string(PADDING, ' ');
+                }
+                out << "|" << endl;
+            }
+            out << line << endl;
             return out;
         }
 };
-//string normalize(string s, unsigned n);
-//string str(Table table);
 
 template <class Printable>
 Table<Printable>::Table(vector<Printable> h, vector<vector<Printable>> c):
@@ -62,4 +77,16 @@ void Table<Printable>::setHeader(vector<Printable> h) {
 template <class Printable>
 void Table<Printable>::setContent(vector<vector<Printable>> c) {
     content = c;
+}
+
+template <class Printable>
+string normalize(const Printable s, unsigned n) {
+    ostringstream aux;
+    aux << s;
+    unsigned diff = n - aux.str().length();
+    ostringstream result;
+    result << string(diff/2 + diff % 2, ' ');
+    result << s;
+    result << string(diff/2, ' ');
+    return result.str();
 }
