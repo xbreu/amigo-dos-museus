cf2033e554eb169f22b8a20136f078e15a4d1a41
Alexandre Abreu <up201800168@up.pt>
2019-11-08 18:17:36 +0000

    Events are not cagados anymore

--- Event/Event.cpp
+++ Event/Event.cpp
@@ -4,15 +4,13 @@
 
 using namespace std;
 
-Event::Event(Museum mus, Date d, float p, string name = ""): museum(move(mus)), date(d), price(p) {
-    this->soldTickets={};
-}
+Event::Event(Museum * museum, Date date, float price, string name): museum(museum), date(date), price(price), name(move(name)) {}
 
 string Event::getName() {
     return this->name;
 }
 
-Museum Event::getMuseum() {
+Museum * Event::getMuseum() {
     return this->museum;
 }
 
@@ -20,7 +18,7 @@ Date Event::getDate() {
     return this->date;
 }
 
-vector<Ticket> Event::getSoldTickets() {
+vector<Ticket*> Event::getSoldTickets() {
     return this->soldTickets;
 }
 
@@ -32,35 +30,43 @@ void Event::setName(string nm) {
     this->name = move(nm);
 }
 
-void Event::setMuseum(Museum mus) {
-    this->museum = move(mus);
+void Event::setMuseum(Museum * museum) {
+    this->museum = museum;
 }
 
 void Event::setDate(Date d) {
     this-> date = d;
 }
 
-void Event::setSoldTickets(vector<Ticket> sold) {
-    this->soldTickets = sold;
-}
-
 void Event::setPrice(float p) {
     this->price = p;
 }
 
-void Event::sellTicket(unsigned int quantity = 1) {
-    if (this->soldTickets > this->museum.getCapacity()) throw OverBookedEvent(this->museum, this->soldTickets);
-    this->soldTickets += quantity;
+Ticket * Event::sellTicket(Person *person) {
+    if (this->soldTickets.size() >= this->museum->getCapacity()) throw OverBookedEvent(this->museum, this->soldTickets.size());
+    auto * aux = new Ticket(this, person);
+    this->soldTickets.push_back(aux);
+    return aux;
 }
 
 ostream & operator<<(ostream & out, const Event & event) {
-    out << event.name << event.date << event.soldTickets << event.price << event.museum;
+    out << event.name << event.date << event.soldTickets.size() << event.price << event.museum;
     return out;
 }
 
 istream &operator>>(istream &in, Event &event) {
-    in >> event.name >> event.date >> event.soldTickets >> event.price >> event.museum;
+    in >> event.name >> event.date >> event.price >> *event.museum;
     return in;
+}
+
+Ticket::Ticket(Event *event, Person *person):person(person),event(event) {}
+
+const Event *Ticket::getEvent() const {
+    return this->event;
+}
+
+const Person *Ticket::getPerson() const {
+    return this->person;
 }
 
 
--- Event/Event.h
+++ Event/Event.h
@@ -5,30 +5,40 @@
 #include "../Date/Date.h"
 #include "iostream"
 #include "../Person/Person.h"
-#include "../Ticket/Ticket.h"
+
+class Event;
+
+class Ticket{
+private:
+    const Event *event ;
+    const Person *person;
+public:
+    Ticket(Event * event, Person * person);
+    const Event *getEvent() const;
+    const Person *getPerson() const;
+};
 
 class Event{
     string name;            //so se da nome ao event ao adiciona-lo ao sistema para ver se ha dois iguais
-    Museum museum;
+    Museum * museum;
     Date date;
-    vector<Ticket> soldTickets;
+    vector<Ticket*> soldTickets;
     float price;
 
 
     public:
-    Event(Museum mus, Date d, float p, string name);  //por data como soldTickets(nao me lembro do nome)
+    Event(Museum * mus, Date date, float price, string name);  //por data como soldTickets(nao me lembro do nome)
     string getName();
-    Museum getMuseum();
+    Museum * getMuseum();
     Date getDate();
-    vector<Ticket> getSoldTickets();
+    vector<Ticket*> getSoldTickets();
     float getPrice();
     void setName(string nm);
-    void setMuseum(Museum mus);
+    void setMuseum(Museum * museum);
     void setDate(Date d);
-    void setSoldTickets(vector<Ticket> sold);
     void setPrice(float p);
 
-    void sellTicket(unsigned quantity);
+    Ticket * sellTicket(Person * person);
 
     friend ostream & operator<<(ostream & out, const Event & event);
 
@@ -38,14 +48,14 @@ class Event{
 
 class OverBookedEvent : exception {
 private:
-    Museum museum;
+    Museum * museum;
     unsigned soldTickets;
 public:
-    OverBookedEvent(Museum museum, unsigned soldTickets) : museum(move(museum)), soldTickets(soldTickets) {}
-    Museum getMuseum() {return museum;}
+    OverBookedEvent(Museum * museum, unsigned soldTickets) : museum(museum), soldTickets(soldTickets) {}
+    Museum * getMuseum() {return museum;}
     unsigned getSoldTickets() {return soldTickets;}
     friend ostream & operator<<(ostream& out, const OverBookedEvent& ev) {
-        out << "OverBookedEvent\nMuseum: " << ev.museum << "\nSoldTickets" << ev.soldTickets;
+        out << "OverBookedEvent\nMuseum: " << * ev.museum << "\nSoldTickets" << ev.soldTickets;
         return out;
     }
 };
