7f927df30c9edcae4142633c319c2cdac4d87c63
Alexandre Abreu <up201800168@up.pt>
2019-11-15 19:17:36 +0000

    Comments

--- docs/output/html/search/all_8.js
+++ docs/output/html/search/all_8.js
@@ -1,12 +1,12 @@
 var searchData =
     [
-        ['individualclient_66', ['IndividualClient', ['../class_individual_client.html', 1, '']]],
-        ['inputaddress_67', ['inputAddress', ['../class_system.html#abd6f9b5932b91d8f541c20383f70a248', 1, 'System']]],
-        ['invalidaddress_68', ['InvalidAddress', ['../class_invalid_address.html', 1, 'InvalidAddress'], ['../class_invalid_address.html#aa67b7ee3db52e9f6ee89bd8c717c1d6d', 1, 'InvalidAddress::InvalidAddress()=default'], ['../class_invalid_address.html#aef0fa1f6cda9981e4901a9a0978fe798', 1, 'InvalidAddress::InvalidAddress(string street, string postalCode, unsigned short doorNumber, string locality)']]],
-        ['invaliddate_69', ['InvalidDate', ['../class_invalid_date.html', 1, '']]],
-        ['invalidevent_70', ['InvalidEvent', ['../class_invalid_event.html', 1, 'InvalidEvent'], ['../class_invalid_event.html#a94f5fc900fb04f1d6e0d6ea4a9cc91a4', 1, 'InvalidEvent::InvalidEvent()']]],
-        ['invalidinput_71', ['InvalidInput', ['../class_invalid_input.html', 1, 'InvalidInput'], ['../class_invalid_input.html#a4f702987230dcf8486e3834f68d4612c', 1, 'InvalidInput::InvalidInput(std::string msg)'], ['../class_invalid_input.html#aaa8b2ee65231eeb253acbfcaae0cf741', 1, 'InvalidInput::InvalidInput()=default']]],
-        ['invalidmuseum_72', ['InvalidMuseum', ['../class_invalid_museum.html', 1, 'InvalidMuseum'], ['../class_invalid_museum.html#abb99197b9195fa0d5db67032dfc235db', 1, 'InvalidMuseum::InvalidMuseum()']]],
-        ['invalidtime_73', ['InvalidTime', ['../class_invalid_time.html', 1, '']]],
-        ['isvalid_74', ['isValid', ['../class_museum.html#a525c002869bf803e13b4c1827ca3e7bb', 1, 'Museum']]]
+            ['individualclient_66', ['IndividualClient', ['../class_individual_client.html', 1, '']]],
+            ['inputaddress_67', ['inputAddress', ['../class_system.html#abd6f9b5932b91d8f541c20383f70a248', 1, 'System']]],
+            ['invalidaddress_68', ['InvalidAddress', ['../class_invalid_address.html', 1, 'InvalidAddress'], ['../class_invalid_address.html#aa67b7ee3db52e9f6ee89bd8c717c1d6d', 1, 'InvalidAddress::InvalidAddress()=default'], ['../class_invalid_address.html#aef0fa1f6cda9981e4901a9a0978fe798', 1, 'InvalidAddress::InvalidAddress(string street, string postalCode, unsigned short doorNumber, string locality)']]],
+            ['invaliddate_69', ['InvalidDate', ['../class_invalid_date.html', 1, '']]],
+            ['invalidevent_70', ['InvalidEvent', ['../class_invalid_event.html', 1, 'InvalidEvent'], ['../class_invalid_event.html#a94f5fc900fb04f1d6e0d6ea4a9cc91a4', 1, 'InvalidEvent::InvalidEvent()']]],
+            ['invalidinput_71', ['InvalidInput', ['../class_invalid_input.html', 1, 'InvalidInput'], ['../class_invalid_input.html#a4f702987230dcf8486e3834f68d4612c', 1, 'InvalidInput::InvalidInput(std::string msg)'], ['../class_invalid_input.html#aaa8b2ee65231eeb253acbfcaae0cf741', 1, 'InvalidInput::InvalidInput()=default']]],
+            ['invalidmuseum_72', ['InvalidMuseum', ['../class_invalid_museum.html', 1, 'InvalidMuseum'], ['../class_invalid_museum.html#abb99197b9195fa0d5db67032dfc235db', 1, 'InvalidMuseum::InvalidMuseum()']]],
+            ['invalidtime_73', ['InvalidTime', ['../class_invalid_time.html', 1, '']]],
+            ['isvalid_74', ['isValid', ['../class_museum.html#a525c002869bf803e13b4c1827ca3e7bb', 1, 'Museum']]]
     ];
--- docs/output/html/search/functions_6.js
+++ docs/output/html/search/functions_6.js
@@ -1,9 +1,9 @@
 var searchData =
     [
-        ['inputaddress_250', ['inputAddress', ['../class_system.html#abd6f9b5932b91d8f541c20383f70a248', 1, 'System']]],
-        ['invalidaddress_251', ['InvalidAddress', ['../class_invalid_address.html#aa67b7ee3db52e9f6ee89bd8c717c1d6d', 1, 'InvalidAddress::InvalidAddress()=default'], ['../class_invalid_address.html#aef0fa1f6cda9981e4901a9a0978fe798', 1, 'InvalidAddress::InvalidAddress(string street, string postalCode, unsigned short doorNumber, string locality)']]],
-        ['invalidevent_252', ['InvalidEvent', ['../class_invalid_event.html#a94f5fc900fb04f1d6e0d6ea4a9cc91a4', 1, 'InvalidEvent']]],
-        ['invalidinput_253', ['InvalidInput', ['../class_invalid_input.html#a4f702987230dcf8486e3834f68d4612c', 1, 'InvalidInput::InvalidInput(std::string msg)'], ['../class_invalid_input.html#aaa8b2ee65231eeb253acbfcaae0cf741', 1, 'InvalidInput::InvalidInput()=default']]],
-        ['invalidmuseum_254', ['InvalidMuseum', ['../class_invalid_museum.html#abb99197b9195fa0d5db67032dfc235db', 1, 'InvalidMuseum']]],
-        ['isvalid_255', ['isValid', ['../class_museum.html#a525c002869bf803e13b4c1827ca3e7bb', 1, 'Museum']]]
+            ['inputaddress_250', ['inputAddress', ['../class_system.html#abd6f9b5932b91d8f541c20383f70a248', 1, 'System']]],
+            ['invalidaddress_251', ['InvalidAddress', ['../class_invalid_address.html#aa67b7ee3db52e9f6ee89bd8c717c1d6d', 1, 'InvalidAddress::InvalidAddress()=default'], ['../class_invalid_address.html#aef0fa1f6cda9981e4901a9a0978fe798', 1, 'InvalidAddress::InvalidAddress(string street, string postalCode, unsigned short doorNumber, string locality)']]],
+            ['invalidevent_252', ['InvalidEvent', ['../class_invalid_event.html#a94f5fc900fb04f1d6e0d6ea4a9cc91a4', 1, 'InvalidEvent']]],
+            ['invalidinput_253', ['InvalidInput', ['../class_invalid_input.html#a4f702987230dcf8486e3834f68d4612c', 1, 'InvalidInput::InvalidInput(std::string msg)'], ['../class_invalid_input.html#aaa8b2ee65231eeb253acbfcaae0cf741', 1, 'InvalidInput::InvalidInput()=default']]],
+            ['invalidmuseum_254', ['InvalidMuseum', ['../class_invalid_museum.html#abb99197b9195fa0d5db67032dfc235db', 1, 'InvalidMuseum']]],
+            ['isvalid_255', ['isValid', ['../class_museum.html#a525c002869bf803e13b4c1827ca3e7bb', 1, 'Museum']]]
     ];
