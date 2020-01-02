753f82ed9ba1dba726f7f958f9a0341c5d278c15
Joao <joaocastropintoz@gmail.com>
2020-01-02 17:51:07 +0000

    fixes

--- code/System/System.cpp
+++ code/System/System.cpp
@@ -1030,13 +1030,14 @@ Table<string> toTable(const vector<Event *> &container, const System * sys){
 }
 
 Table<string> toTable(const vector<Client *> &container, const System *sys) {
-    vector<string> header = {"Name", "Birthday", "Address", "Contact"};
+    vector<string> header = {"Type", "Name", "Birthday", "Address", "Contact"};
     vector<vector<string>> content;
     for (auto client : container) {
         stringstream address, birthday;
         address << client->getAddress();
         birthday << client->getBirthday();
-        vector<string> aux = {client->getName(), birthday.str(), address.str(), to_string(client->getContact())};
+        vector<string> aux = {client->getType(), client->getName(), birthday.str(), address.str(),
+                              to_string(client->getContact())};
         content.push_back(aux);
     }
     Table<string> data(header, content);
